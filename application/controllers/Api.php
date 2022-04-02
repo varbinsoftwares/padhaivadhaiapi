3e5475ba0147562d7d8ccf0e94260cb9c1edc04a
octopuscart <dev@develop.com>
2022-04-02 17:00:05 +0530

    changes files

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -312,7 +312,7 @@ class Api extends REST_Controller {
 
         $actfilname = $_FILES['file']['name'];
 
-        $filelocation = APPPATH . "../../../bookbnev/public_html/app.padhaivadhai.com/padhaiVadhaiApp/original/";
+        $filelocation = APPPATH . "../../app_padhaivadhai_com/padhaiVadhaiApp/original/";
         move_uploaded_file($_FILES["file"]['tmp_name'], $filelocation . $actfilname);
 
 
